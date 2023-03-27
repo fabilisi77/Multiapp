9e8a02e41c5b9cd6cafe419fdc0f7c45b03ef233
Fabian <fabian_carrazan@hotmail.com>
2023-03-27 16:45:45 -0300

    pantalla ICM app

--- app/src/main/java/com/example/primeraapp/MenuActivity.kt
+++ app/src/main/java/com/example/primeraapp/MenuActivity.kt
@@ -3,22 +3,34 @@ package com.example.primeraapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.widget.AppCompatButton
+import com.example.primeraapp.imcCalculator.ImcAppActivity
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
 
-        val saludapp = findViewById<AppCompatButton>(R.id.btnSaludapp)
+        val saludapp = findViewById<Button>(R.id.btnSaludapp)
+        val imcApp = findViewById<Button>(R.id.btnImcApp)
+
         saludapp.setOnClickListener {
             navigateToSaludapp()
-
+        }
+        imcApp.setOnClickListener {
+            navigateToImcapp()
         }
     }
 
     private fun navigateToSaludapp(){
         val intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
+
+    }
+
+    private fun navigateToImcapp(){
+        val intent = Intent (this, ImcAppActivity::class.java)
         startActivity(intent)
 
     }
--- app/src/main/java/com/example/primeraapp/imcCalculator/ImcAppActivity.kt
+++ app/src/main/java/com/example/primeraapp/imcCalculator/ImcAppActivity.kt
@@ -65,6 +65,7 @@ class ImcAppActivity : AppCompatActivity() {
     }
 
     private fun initUI(){
+        setGenderColor()
 
     }
 }
