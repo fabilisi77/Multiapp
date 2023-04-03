8dd7d08a2c8f9ef7eed0330ad5dd74699277cd8e
Fabian <fabian_carrazan@hotmail.com>
2023-04-02 21:25:04 -0300

    commit

--- app/src/main/java/com/example/multiapp/appTodo/AppTodoActivity.kt
+++ app/src/main/java/com/example/multiapp/appTodo/AppTodoActivity.kt
@@ -1,10 +1,12 @@
 package com.example.multiapp.appTodo
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.primeraapp.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class AppTodoActivity : AppCompatActivity() {
@@ -15,37 +17,55 @@ class AppTodoActivity : AppCompatActivity() {
         TaskCategory.Personal
     )
 
-    private val tasks = mutableListOf (
-        Task("Prubea Businnes",TaskCategory.Business),
-        Task("Prubea Personal",TaskCategory.Personal),
-        Task("Prubea Other",TaskCategory.Other)
-
+    private val tasks = mutableListOf(
+        Task("Prubea Businnes", TaskCategory.Business),
+        Task("Prubea Personal", TaskCategory.Personal),
+        Task("Prubea Other", TaskCategory.Other)
 
 
     )
     private lateinit var rvCategories: RecyclerView
     private lateinit var categoriesAdapter: CategoriesAdapter
-    private lateinit var rvTask:RecyclerView
+    private lateinit var rvTask: RecyclerView
     private lateinit var taskAdapter: TaskAdapter
+    private lateinit var fabAddTask: FloatingActionButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_app_todo)
 
         initComponents()
         initUI()
+        initListeners()
+    }
+
+    private fun initListeners() {
+        fabAddTask.setOnClickListener {
+            showDialog()
+        }
+    }
+
+    private fun showDialog(){
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.dialog_task)
+        dialog.show()
     }
 
     private fun initComponents() {
         rvCategories = findViewById(R.id.rvCategories)
         rvTask = findViewById(R.id.rvTask)
+        fabAddTask = findViewById(R.id.fabAddTask)
     }
 
-    private fun initUI(){
+    private fun initUI() {
         categoriesAdapter = CategoriesAdapter(categories)
-        rvCategories.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
+        rvCategories.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         rvCategories.adapter = categoriesAdapter
 
         taskAdapter = TaskAdapter(tasks)
+        rvTask.layoutManager = LinearLayoutManager(this)
+        /**Para que se ves en forma vertical,solo hay que psasr un "this"**/
+        rvTask.adapter = taskAdapter
 
 
     }
